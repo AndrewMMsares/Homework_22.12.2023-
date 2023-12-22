6c7ac6a5aa9c4a7d3ca79abe3aa37397f268cf48
Andrei Kalinichenko <muse2140@gmail.com>
2023-12-22 23:51:30 +0200

    Basic

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+# Task 1
+# print("Hello user")
+# string = input("Please enter string: ")
+# letter = 0
+# digit = 0
+# try:
+#     for i in string:
+#         if i.isalpha():
+#             letter += 1
+#         elif i.isdigit():
+#             digit += 1
+# except Exception as error:
+#     print()
+#
+# print("Letter: ", letter)
+# print("Digit: ", digit)
+
 # Task 2
 # print("Hello user")
 # string = input("Please enter string: ")
@@ -36,6 +53,7 @@
 # last_string = ' '.join(words)
 # print(last_string)
 
+
 # Task 4
 # string = input("Enter the line ")
 # print(string[2])
@@ -45,3 +63,20 @@
 # print(string[1::2])
 # print(string[::-1])
 # print(string[::-2])
+
+# Task 5
+# input_text = input("Enter the line ")
+#
+# sentences = input_text.split('. ')
+# processed_text = '. '.join(sentence.capitalize() for sentence in sentences)
+#
+# digit = sum(c.isdigit() for c in input_text)
+# punctuation = sum(c in ',.?!;:-()"\'[]{}' for c in input_text)
+# exclamation = input_text.count('!')
+#
+# print("Changed text:")
+# print(processed_text)
+#
+# print("\nThe number of digits in the text:", digit)
+# print("The number of punctuation marks in the text:", punctuation)
+# print("The number of exclamation marks in the text:", exclamation)
