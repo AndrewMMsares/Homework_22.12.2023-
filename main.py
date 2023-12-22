8f3b73133236d7af5cc124d2ee7e8878860b3d71
Andrei Kalinichenko <muse2140@gmail.com>
2023-12-22 23:43:58 +0200

    Task 2

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# Task 2
+# print("Hello user")
+# string = input("Please enter string: ")
+# search = input("Please enter letter: ")
+#
+# count = 0
+#
+# for i in string:
+#     if i == search:
+#         count += 1
+#
+# print("symbol: ", search, "\nmeets: ", count)
 
