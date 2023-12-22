c02ae330147098489a1e4bd5cb58c81881ed8158
Andrei Kalinichenko <muse2140@gmail.com>
2023-12-22 23:44:26 +0200

    Task 3

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+# Task 3.1
+# string = input("Enter the line ")
+# word = input("Enter a word to search for: ")
+# replace_word = input("Enter a replacement word: ")
+#
+# string2 = string.replace(word, replace_word)
+#
+# print(string2)
+
+
+# Task 3.2
+# string = input("Enter the line ")
+# word = input("Enter a word to search for: ")
+# replace_word = input("Enter a replacement word: ")
+#
+# words = string.split()
+#
+# for i in range(len(words)):
+#     if words[i] == word:
+#         words[i] = replace_word
+#
+# last_string = ' '.join(words)
+# print(last_string)
 
