b1454ac5aa2877fe589c3bac3ca871f0d62b5793
Andrei Kalinichenko <muse2140@gmail.com>
2023-12-22 23:44:41 +0200

    Task 4

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+# Task 4
+# string = input("Enter the line ")
+# print(string[2])
+# print(string[-2])
+# print(string[:5])
+# print(string[::2])
+# print(string[1::2])
+# print(string[::-1])
+# print(string[::-2])
