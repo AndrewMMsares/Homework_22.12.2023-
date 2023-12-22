f028575738fef0ee8ec4dd4f09cc6bc54cb902b3
Andrei Kalinichenko <muse2140@gmail.com>
2023-12-22 23:45:51 +0200

    Task 5

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-
+# Task 5
+# input_text = input("Enter the line ")
+#
+# sentences = input_text.split('. ')
+# processed_text = '. '.join(sentence.capitalize() for sentence in sentences)
+#
+# digit = sum(c.isdigit() for c in input_text)
+# punctuation = sum(c in ',.?!;:-()"\'[]{}' for c in input_text)
+# exclamation = input_text.count('!')
+#
+# print("Changed text:")
+# print(processed_text)
+#
+# print("\nThe number of digits in the text:", digit)
+# print("The number of punctuation marks in the text:", punctuation)
+# print("The number of exclamation marks in the text:", exclamation)
