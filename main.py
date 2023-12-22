b8278c1f626d4da490b5e77ed7d77fdf4673b2c8
Andrei Kalinichenko <muse2140@gmail.com>
2023-12-22 23:43:11 +0200

    Task 1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-
+# Task 1
+# print("Hello user")
+# string = input("Please enter string: ")
+# letter = 0
+# digit = 0
+# try:
+#     for i in string:
+#         if i.isalpha():
+#             letter += 1
+#         elif i.isdigit():
+#             digit += 1
+# except Exception as error:
+#     print()
+#
+# print("Letter: ", letter)
+# print("Digit: ", digit)
